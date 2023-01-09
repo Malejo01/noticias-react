3ba992f2d96354b0c25f4458043af78399cd690b
Malejo01 <lizarragamauroalejandro@gmail.com>
2023-01-09 00:16:28 -0300

    Noticias react

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import {Container, Grid, Typography} from '@mui/material'
+import Formulario from './components/Formulario'
+import { NoticiasProvider } from './context/noticiasprovider'
+
+function App() {
+  
+
+  return (
+    <NoticiasProvider>
+        <Container>
+          <header>
+              <Typography align='center' marginY={5} component="h1" variant='h1'> Buscador de Noticias</Typography>
+          </header>
+          <Grid 
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center">
+            <Grid item  xs={12} md={6}>
+              <Formulario/>
+            </Grid>
+          </Grid>
+        </Container>
+      </NoticiasProvider>
+  )
+}
+
+export default App
